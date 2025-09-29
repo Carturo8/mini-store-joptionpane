dabd367a4cd01909afd1f593ce134f977d4e21d1
Carturo8 <caarojasbo@unal.edu.co>
2025-09-28 20:03:17 -0500

    feat: add Product.java model class for product representation

---
+++ src/main/java/org/carturo/ministore/model/Product.java
@@ -0,0 +1,36 @@
+package org.carturo.ministore.model;
+
+public class Product {
+    private String name;
+    private double price;
+    private int stock;
+
+    public Product(String name, double price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        if (stock >= 0) {
+            this.stock = stock;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s | Price: %.2f | Stock: %d", name, price, stock);
+    }
+}
